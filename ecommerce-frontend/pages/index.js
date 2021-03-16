434f877655997b106d2a55a435b327992698098b
Patryk Adach <patrykadachofficial@gmail.com>
2021-03-17 00:17:29 +0100

    Added single product page

--- ecommerce-frontend/pages/index.js
+++ ecommerce-frontend/pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
-
+import Link from 'next/link'
+import { fromImageToUrl } from '../utils/urls'
 import products from '../products';
+
+import {twoDecimals} from '../utils/format';
 
 export default function Home() {
   return (
@@ -10,20 +13,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="grid grid-flow-row grid-cols-4 grid-rows-3 gap-4">
-      {products.map((product) => (
-        <div key={product.name} className="p-10 bg-gray-100 rounded-lg text-black shadow-sm hover:bg-gray-200 cursor-pointer">
-          <div className="mb-10">
-            <img className="rounded-lg object-cover h-48 w-full" src={`http://localhost:1337${product.image.url}`} />
-          </div>
-          <div className="text-base">
-            <div className="font-bold">{product.name}</div> 
-            <div className="text-gray-500">{product.price} PLN</div>
-          </div>
-          
-          
-        </div> 
-      ))}
+      <div className="container mx-auto grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {products.map((product) => ( 
+          <div key={product.name} className="p-10 bg-gray-100 rounded-lg text-black shadow-sm hover:bg-gray-200 cursor-pointer">
+            <Link href={`/products/${product.slug}`}>
+              <a>
+              <div className="mb-10">
+                <img className="rounded-lg object-cover h-48 w-full" src={fromImageToUrl(product.image)} />
+              </div>
+              <div className="text-base">
+                <div className="font-bold">{product.name}</div> 
+                <div className="text-gray-500">{twoDecimals(product.price)} PLN</div>
+              </div>
+              </a>
+            </Link>
+          </div> 
+        ))}
       </div>
       
     </div>
