024e3d35683a512e328ccda00696a27cb2c1734d
Patryk Adach <patrykadachofficial@gmail.com>
2021-03-16 23:39:06 +0100

    Added example product.json and products grid styling

--- ecommerce-frontend/pages/index.js
+++ ecommerce-frontend/pages/index.js
@@ -1,65 +1,31 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
+import products from '../products';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className="grid grid-flow-row grid-cols-4 grid-rows-3 gap-4">
+      {products.map((product) => (
+        <div key={product.name} className="p-10 bg-gray-100 rounded-lg text-black shadow-sm hover:bg-gray-200 cursor-pointer">
+          <div className="mb-10">
+            <img className="rounded-lg object-cover h-48 w-full" src={`http://localhost:1337${product.image.url}`} />
+          </div>
+          <div className="text-base">
+            <div className="font-bold">{product.name}</div> 
+            <div className="text-gray-500">{product.price} PLN</div>
+          </div>
+          
+          
+        </div> 
+      ))}
+      </div>
+      
     </div>
   )
 }
