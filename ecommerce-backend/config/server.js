64092e0bd178ad875956471ac7a517aba0033f40
Patryk Adach <patrykadachofficial@gmail.com>
2021-03-16 22:51:37 +0100

    Updated backend and added frontend boilerplate code

---
+++ ecommerce-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '415a64d3175974ab3aa93ab1f0423026'),
+    },
+  },
+});
---
+++ ecommerce-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '88475f16-060f-4d13-a2d5-4631276ce39d'
+};
