3870df8539c8dd22960219dec796ba3838c400f4
Patryk Adach <patrykadachofficial@gmail.com>
2021-03-18 00:07:38 +0100

    Added styles to header

--- ecommerce-frontend/components/Header.js
+++ ecommerce-frontend/components/Header.js
@@ -12,8 +12,9 @@ export default () => {
     }
 
     return (
-        <div className="text-xl p-10 bg-gray-100 mb-20">
-            {!isHome && <> Go back</>}
+        <div className="text-xl p-10 bg-gray-100 mb-20 flex justify-center">
+            {!isHome && 
+                <div className="absolute left-5"><a href="#" onClick={goBack}> {"<"} Go back</a></div>}
             <Link href="/">
                 <a>
                     <h1>
